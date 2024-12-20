aea5f8e1a2210599b84e72787b2ce186088d72d6
Lucas Bonny <lucasbonnyb8@gmail.com>
2024-12-20 20:30:19 -0300

    Project created

---
+++ src/main/java/br/com/gunthercloud/telegramapi/TelegramapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gunthercloud.telegramapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramapiApplication.class, args);
+	}
+
+}
